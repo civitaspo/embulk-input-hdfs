c73175b5e8998c352b8c3198862c5a1322d43861
Civitaspo <civitaspo@gmail.com>
2015-08-19 17:54:38 +0900

    v0.1.0 init

--- lib/embulk/input/hdfs.rb
+++ lib/embulk/input/hdfs.rb
@@ -1,3 +1,3 @@
 Embulk::JavaPlugin.register_input(
-  "hdfs", "org.embulk.input.HdfsFileInputPlugin",
+  "hdfs", "org.embulk.input.hdfs.HdfsFileInputPlugin",
   File.expand_path('../../../../classpath', __FILE__))
