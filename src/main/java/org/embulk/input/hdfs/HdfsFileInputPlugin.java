96ce09deb610e73c75f62e8e0bac1e90aae4e1e5
Civitaspo <civitaspo@gmail.com>
2015-08-20 10:01:47 +0900

    simplify the code

--- src/main/java/org/embulk/input/hdfs/HdfsFileInputPlugin.java
+++ src/main/java/org/embulk/input/hdfs/HdfsFileInputPlugin.java
@@ -1,9 +1,17 @@
 package org.embulk.input.hdfs;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
+
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.embulk.config.TaskReport;
 import org.embulk.config.Config;
 import org.embulk.config.ConfigDefault;
@@ -16,38 +24,37 @@ import org.embulk.spi.Exec;
 import org.embulk.spi.FileInputPlugin;
 import org.embulk.spi.BufferAllocator;
 import org.embulk.spi.TransactionalFileInput;
+import org.embulk.spi.util.InputStreamFileInput;
 import org.embulk.spi.util.InputStreamTransactionalFileInput;
+import org.jruby.embed.ScriptingContainer;
+import org.slf4j.Logger;
 
-public class HdfsFileInputPlugin
-        implements FileInputPlugin
+public class HdfsFileInputPlugin implements FileInputPlugin
 {
-    public interface PluginTask
-            extends Task
+    private static final Logger logger = Exec.getLogger(HdfsFileInputPlugin.class);
+
+    public interface PluginTask extends Task
     {
-        // configuration option 1 (required integer)
-        @Config("option1")
-        public int getOption1();
+        @Config("config_files")
+        @ConfigDefault("[]")
+        public List<String> getConfigFiles();
 
-        // configuration option 2 (optional string, null is not allowed)
-        @Config("optoin2")
-        @ConfigDefault("\"myvalue\"")
-        public String getOption2();
+        @Config("config")
+        @ConfigDefault("{}")
+        public Map<String, String> getConfig();
 
-        // configuration option 3 (optional string, null is allowed)
-        @Config("optoin3")
-        @ConfigDefault("null")
-        public Optional<String> getOption3();
+        @Config("input_path")
+        public String getInputPath();
 
-        //@Config("path_prefix")
-        //public String getPathPrefix();
+        @Config("rewind_seconds")
+        @ConfigDefault("0")
+        public int getRewindSeconds();
 
-        //@Config("last_path")
-        //@ConfigDefault("null")
-        //public Optional<String> getLastPath();
+        public List<Path> getHdfsFiles();
+        public void setHdfsFiles(List<Path> hdfsFiles);
 
-        // usually, you store list of files in task to pass them from transaction() to run().
-        //public List<String> getFiles();
-        //public void setFiles(List<String> files);
+        public FileSystem getHdfs();
+        public void setHdfs(FileSystem hdfs);
 
         @ConfigInject
         public BufferAllocator getBufferAllocator();
@@ -58,8 +65,29 @@ public class HdfsFileInputPlugin
     {
         PluginTask task = config.loadConfig(PluginTask.class);
 
-        // run() method is called for this number of times in parallel.
-        int taskCount = 1;
+        // prepare: set FileSystem
+        try {
+            Configuration configuration = getHdfsConfiguration(task);
+            FileSystem hdfs = FileSystem.get(configuration);
+            task.setHdfs(hdfs);
+        }
+        catch (IOException e) {
+            logger.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        // listing Files
+        String pathString = strftime(task.getInputPath(), task.getRewindSeconds());
+        try {
+            task.setHdfsFiles(buildFileList(task, pathString));
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        logger.info("Loading target files: {}", task.getHdfsFiles());
+
+        // number of processors is same with number of targets
+        int taskCount = task.getHdfsFiles().size();
 
         // usually, taskCount is number of input files.
         //task.setFiles(listFiles(task));
@@ -81,10 +109,71 @@ public class HdfsFileInputPlugin
     //    return builder.build();
     //}
 
+    private Configuration getHdfsConfiguration(final PluginTask task)
+    {
+        Configuration configuration = new Configuration();
+
+        for (Object configFile : task.getConfigFiles()) {
+            configuration.addResource(configFile.toString());
+        }
+        configuration.reloadConfiguration();
+
+        for (Map.Entry<String, String> entry: task.getConfig().entrySet()) {
+            configuration.set(entry.getKey(), entry.getValue());
+        }
+
+        return configuration;
+    }
+
+    private String strftime(final String raw, final int rewind_seconds)
+    {
+        ScriptingContainer jruby = new ScriptingContainer();
+        Object resolved = jruby.runScriptlet(
+                String.format("(Time.now - %s).strftime('%s')", String.valueOf(rewind_seconds), raw));
+        return resolved.toString();
+    }
+
+    private List<Path> buildFileList(final PluginTask task, final String pathString) throws IOException
+    {
+        List<Path> fileList = new ArrayList<>();
+        for (FileStatus entry : task.getHdfs().globStatus(new Path(pathString))) {
+            if (entry.isDirectory()) {
+                fileList.addAll(lsr(task.getHdfs(), entry));
+            } else {
+                fileList.add(entry.getPath());
+            }
+        }
+        return fileList;
+    }
+
+    private List<Path> lsr(final FileSystem fs, FileStatus status) throws IOException
+    {
+        List<Path> fileList = new ArrayList<>();
+        if (status.isDirectory()) {
+            for (FileStatus entry : fs.listStatus(status.getPath())) {
+                fileList.addAll(lsr(fs, entry));
+            }
+        }
+        else {
+            fileList.add(status.getPath());
+        }
+        return fileList;
+    }
+
+//    private List<Path> distinctFileList(List<Path> fileList)
+//    {
+//        Set<Path> set = new HashSet<Path>();
+//        set.addAll(fileList);
+//        List<Path> uniqueFileList = new ArrayList<Path>();
+//        uniqueFileList.addAll(set);
+//        return uniqueFileList;
+//    }
+
+
     @Override
     public ConfigDiff resume(TaskSource taskSource,
-            int taskCount,
-            FileInputPlugin.Control control)
+                             int taskCount,
+                             FileInputPlugin.Control control)
     {
         control.run(taskSource, taskCount);
 
@@ -106,8 +195,8 @@ public class HdfsFileInputPlugin
 
     @Override
     public void cleanup(TaskSource taskSource,
-            int taskCount,
-            List<TaskReport> successTaskReports)
+                        int taskCount,
+                        List<TaskReport> successTaskReports)
     {
     }
 
@@ -116,28 +205,28 @@ public class HdfsFileInputPlugin
     {
         final PluginTask task = taskSource.loadTask(PluginTask.class);
 
-        // Write your code here :)
-        throw new UnsupportedOperationException("HdfsFileInputPlugin.open method is not implemented yet");
+        InputStream input;
+        try {
+            input = openInputStream(task, task.getHdfsFiles().get(taskIndex));
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
 
-        // if you expect InputStream, you can use this code:
+        return new InputStreamTransactionalFileInput(task.getBufferAllocator(), input) {
+            @Override
+            public void abort()
+            { }
 
-        //InputStream input = openInputStream(task, task.getFiles().get(taskIndex));
-        //
-        //return new InputStreamTransactionalFileInput(task.getBufferAllocator(), input) {
-        //    @Override
-        //    public void abort()
-        //    { }
-        //
-        //    @Override
-        //    public TaskReport commit()
-        //    {
-        //        return Exec.newTaskReport();
-        //    }
-        //}
+            @Override
+            public TaskReport commit()
+            {
+                return Exec.newTaskReport();
+            }
+        };
     }
 
-    //private static InputStream openInputStream(PluginTask task, String path)
-    //{
-    //    return new MyInputStream(file);
-    //}
+    private static InputStream openInputStream(PluginTask task, Path path) throws IOException {
+        return task.getHdfs().open(path);
+    }
 }
