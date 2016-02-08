e9db52f90b15b179d42e16fcabb94157b79893fe
Civitaspo <civitaspo@gmail.com>
2016-02-08 14:54:30 +0900

    Optimize imports

--- src/main/java/org/embulk/input/hdfs/HdfsFileInputPlugin.java
+++ src/main/java/org/embulk/input/hdfs/HdfsFileInputPlugin.java
@@ -33,7 +33,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.SequenceInputStream;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
